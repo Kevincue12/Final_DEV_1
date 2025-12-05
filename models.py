5c4ee46af323206563bbace1d6345a1fe8ea5a9d
Sala 2 <sala2@FI-LabSistemas.ucatolica.loc>
2025-12-05 10:03:19 -0500

    modelos

--- models.py
+++ models.py
@@ -1,12 +1,80 @@
-class Jugador():
-    pass
+from sqlalchemy import (
+    Column, Integer, String, Float, Date, Enum,
+    Boolean, ForeignKey
+)
+from sqlalchemy.orm import relationship
+from database import Base
+import enum
+
+class EstadoJugador(str, enum.Enum):
+    activo = "activo"
+    lesionado = "lesionado"
+    suspendido = "suspendido"
 
 
-class Estadistica():
-    pass
+class PosicionJugador(str, enum.Enum):
+    portero = "portero"
+    defensa = "defensa"
+    medio = "medio"
+    delantero = "delantero"
 
 
-class Partido():
-    pass
+class ResultadoPartido(str, enum.Enum):
+    victoria = "victoria"
+    empate = "empate"
+    derrota = "derrota"
+
+class Jugador(Base):
+    __tablename__ = "jugadores"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, nullable=False)
+    numero_camiseta = Column(Integer, nullable=False)
+    nacionalidad = Column(String, nullable=False)
+    fecha_nacimiento = Column(Date, nullable=False)
+
+    altura_cm = Column(Float)      
+    peso_kg = Column(Float)      
+    pie_dominante = Column(String)  
+    velocidad = Column(Float, nullable=True)   
+    resistencia = Column(Float, nullable=True) 
+
+    posicion = Column(Enum(PosicionJugador), nullable=False)
+    estado = Column(Enum(EstadoJugador), default=EstadoJugador.activo)
+
+    estadisticas = relationship("EstadisticaJugador", back_populates="jugador")
 
 
+class Partido(Base):
+    __tablename__ = "partidos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    fecha = Column(Date, nullable=False)
+    rival = Column(String, nullable=False)
+    es_local = Column(Boolean, default=True)
+
+    goles_sigomota = Column(Integer, default=0)
+    goles_rival = Column(Integer, default=0)
+
+    resultado = Column(Enum(ResultadoPartido), nullable=False)
+
+    resuelto_por_penales = Column(Boolean, default=False)
+
+    estadisticas = relationship("EstadisticaJugador", back_populates="partido")
+
+
+class EstadisticaJugador(Base):
+    __tablename__ = "estadisticas_jugador"
+
+    id = Column(Integer, primary_key=True, index=True)
+    jugador_id = Column(Integer, ForeignKey("jugadores.id"))
+    partido_id = Column(Integer, ForeignKey("partidos.id"))
+
+    minutos_jugados = Column(Integer, default=0)
+    goles_marcados = Column(Integer, default=0)
+    asistencias = Column(Integer, default=0)
+    tarjetas_amarillas = Column(Integer, default=0)
+    tarjetas_rojas = Column(Integer, default=0)
+
+    jugador = relationship("Jugador", back_populates="estadisticas")
+    partido = relationship("Partido", back_populates="estadisticas")
