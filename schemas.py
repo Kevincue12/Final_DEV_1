079e013df8d3c6c3cce07d6f21c7f84acfce32c4
Sala 2 <sala2@FI-LabSistemas.ucatolica.loc>
2025-12-05 10:05:23 -0500

    schemasssssss

---
+++ schemas.py
@@ -0,0 +1,87 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional, List
+import enum
+
+class EstadoJugadorAPI(str, enum.Enum):
+    activo = "activo"
+    lesionado = "lesionado"
+    suspendido = "suspendido"
+
+
+class PosicionJugadorAPI(str, enum.Enum):
+    portero = "portero"
+    defensa = "defensa"
+    medio = "medio"
+    delantero = "delantero"
+
+
+class ResultadoPartidoAPI(str, enum.Enum):
+    victoria = "victoria"
+    empate = "empate"
+    derrota = "derrota"
+
+class EstadisticaBase(BaseModel):
+    minutos_jugados: int = 0
+    goles_marcados: int = 0
+    asistencias: int = 0
+    tarjetas_amarillas: int = 0
+    tarjetas_rojas: int = 0
+
+
+class EstadisticaCreate(EstadisticaBase):
+    jugador_id: int
+    partido_id: int
+
+
+class Estadistica(EstadisticaBase):
+    id: int
+    jugador_id: int
+    partido_id: int
+
+    model_config = {"from_attributes": True}
+
+class JugadorBase(BaseModel):
+    nombre: str
+    numero_camiseta: int
+    nacionalidad: str
+    fecha_nacimiento: date
+    altura_cm: float
+    peso_kg: float
+    pie_dominante: str
+    posicion: PosicionJugadorAPI
+    estado: EstadoJugadorAPI = EstadoJugadorAPI.activo
+    velocidad: Optional[float] = None
+    resistencia: Optional[float] = None
+
+
+class JugadorCreate(JugadorBase):
+    pass
+
+
+class Jugador(JugadorBase):
+    id: int
+
+    model_config = {"from_attributes": True}
+
+class PartidoBase(BaseModel):
+    fecha: date
+    rival: str
+    es_local: bool = True
+    goles_sigomota: int = 0
+    goles_rival: int = 0
+    resultado: ResultadoPartidoAPI
+    resuelto_por_penales: bool = False
+
+
+class PartidoCreate(PartidoBase):
+    pass
+
+class Partido(PartidoBase):
+    id: int
+
+    model_config = {"from_attributes": True}
+
+class EstadisticaDetallada(Estadistica):
+    jugador: Jugador
+    partido: Partido
